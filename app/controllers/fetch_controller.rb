3ec8a4cdfcc5b33534e7cb81594f63e344c183d1
xryuseix <ryusei143.shootingstar@gmail.com>
2019-12-05 23:16:21 +0900

    submitをsort

--- app/controllers/fetch_controller.rb
+++ app/controllers/fetch_controller.rb
@@ -2,10 +2,19 @@ class FetchController < ApplicationController
   def index
     require 'net/http'
     require 'json'
-
+    
+    # === データを取得 ===
     submit = usersubmit
     contest = contestinfo
     
+    # === データを加工 ===
+    submit.sort! { |a, b| b['epoch_second'] <=> a['epoch_second'] }
+
+    p submit[0]
+    p submit[1]
+    p submit[2]
+    p submit[3]
+
   end
 
   def GetJsonAPI(urlstring)

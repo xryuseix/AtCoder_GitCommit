a2f639ab91158cc161c1dba5cf2e2c8cd293a3fa
xryuseix <ryusei143.shootingstar@gmail.com>
2019-12-07 08:27:17 +0900

    ファイル書き込み処理追加

--- app/controllers/fetch_controller.rb
+++ app/controllers/fetch_controller.rb
@@ -9,23 +9,24 @@ class FetchController < ApplicationController
     submit.sort! { |a, b| b['epoch_second'] <=> a['epoch_second'] }
 
     # === 前日の提出コードを取得 ===
-    # source_codes = []
-    # lastday_submits = lastday_submit_array(time, submit)
+    source_codes = []
+    lastday_submits = lastday_submit_array(time, submit)
   
-    # lastday_submits.each do |problemnum|
-    #   url = "https://atcoder.jp/contests/#{problemnum['contest_id']}/submissions/#{problemnum['id']}"
-    #   source_codes << fetch_source_code(url)
-    # end
+    lastday_submits.each do |i|
+      url = "https://atcoder.jp/contests/#{i['contest_id']}/submissions/#{i['id']}"
+      source_codes << fetch_source_code(url)
+    end
 
     # === 問題文を取得 ===
     problem_statement = []
     
-    # lastday_submits.each do |problemnum|
-    #   url = "https://atcoder.jp/contests/#{problemnum['contest_id']}/submissions/#{problemnum['id']}"
-    #   source_codes << fetch_source_code(url)
-    # end
-    url = "https://atcoder.jp/contests/apc001/tasks/apc001_b"
-    fetch_problem_statement(url)
+    lastday_submits.each do |i|
+      url = "https://atcoder.jp/contests/#{i['contest_id']}/tasks/#{i['problem_id']}"
+      problem_statement << fetch_problem_statement(url)
+    end
+    
+    # ファイルに書き込み
+    file_write(source_codes, problem_statement, lastday_submits)
 
   end
 
@@ -56,10 +57,10 @@ class FetchController < ApplicationController
     return GetJsonAPI("https://kenkoooo.com/atcoder/atcoder-api/results?user=" + username)
   end
 
-  def contestinfo
-    # === コンテストの情報を所得 ===
-    return GetJsonAPI("https://kenkoooo.com/atcoder/resources/problems.json")
-  end
+  # def contestinfo
+  #   # === コンテストの情報を所得 ===
+  #   return GetJsonAPI("https://kenkoooo.com/atcoder/resources/problems.json")
+  # end
 
   def time
     # === 昨日の日付を取得 ===
@@ -107,5 +108,35 @@ class FetchController < ApplicationController
     statement = statement.gsub(/Time Limit/, "\nTime Limit ").gsub(/Memory Limit/, "Memory Limit ").gsub(/配点/, "\n配点").gsub(/問題文/, "\n問題文 : ").gsub(/制約/, "\n制約")
     return statement
   end
+
+  def file_write(source_codes, problem_statement, lastday_submits)
+    # === データ(問題文・ソースコード)を書き込み ===
+
+      # 書き込み先ディレクトリがなかったら作る
+    lastday_submits.length.times do |i|
+      if not is_exist_directory("#{Dir.pwd}/storage/AtCoder/", "#{Dir.pwd}/storage/AtCoder/#{lastday_submits[i]['contest_id']}/")
+        Dir.mkdir("#{Dir.pwd}/storage/AtCoder/#{lastday_submits[i]['contest_id']}")
+      end
+
+      # 書き込み
+      File.open("#{Dir.pwd}/storage/AtCoder/#{lastday_submits[i]['contest_id']}/#{lastday_submits[i]['problem_id']}_#{lastday_submits[i]['id']}.txt", "w") do |f|
+        url = "https://atcoder.jp/contests/#{lastday_submits[i]['contest_id']}/tasks/#{lastday_submits[i]['problem_id']}"
+        f.puts("問題文の引用元：#{url}")
+        f.puts(problem_statement[i])
+        f.puts("\n\n\n")
+        f.puts(source_codes[i])
+      end
+    end
+
+  end
+
+  def is_exist_directory(path, dir)
+    # pathの中にdirというディレクトリがあるか確認する
+    Dir.glob("#{path}*/").each do |directory_name|
+      return true if directory_name == dir
+    end
+    return false
+  end
+
 
 end

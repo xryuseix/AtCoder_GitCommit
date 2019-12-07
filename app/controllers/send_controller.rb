3b301916ae0d6e4120379499aec88d8e65c85ab8
xryuseix <ryusei143.shootingstar@gmail.com>
2019-12-07 17:14:03 +0900

    add send script

--- app/controllers/send_controller.rb
+++ app/controllers/send_controller.rb
@@ -1,12 +1,25 @@
 class SendController < ApplicationController
   def index
 
-    system('cd storage; ls')
-    system('git init')
-    system('git remote add origin "https://github.com/xryuseix/Competitive_Programming_Buckup"')
-    system('git add --all')
-    system('git commit -m "first commit"')
-    system('git push -u origin master')
+    lastday = time
+
+    # This command is used at once.
+    # git init; git remote add origin 'https://github.com/xryuseix/Competitive_Programming_Buckup';
+
+    # This is remove command(for test).
+    # system("cd storage; git rm -rf AtCoder/* ;")
+    # system("cd storage; git add --all; git commit -m 'remove test script'; git push -u origin master")
+
+    system("cd storage; git add --all; git commit -m '#{time.year}/#{time.month}/#{time.day}'; git push -u origin master")
     
   end
+
+  def time
+    # === 昨日の日付を取得 ===
+    now_unixtime = Time.new.to_i
+    lastday_unixtime = now_unixtime - 60*60*24
+    lastday = Time.at(lastday_unixtime)
+    return lastday
+  end
+
 end

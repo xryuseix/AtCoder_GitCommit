4a9778f5180ac7d0c940b8f5e6a4ba3629a455d6
xryuseix <ryusei143.shootingstar@gmail.com>
2019-12-07 16:44:22 +0900

    first commit

--- app/controllers/send_controller.rb
+++ app/controllers/send_controller.rb
@@ -3,7 +3,7 @@ class SendController < ApplicationController
 
     system('cd storage; ls')
     system('git init')
-    system('git remote add origin https://github.com/xryuseix/Competitive_Programming_Buckup')
+    system('git remote add origin "https://github.com/xryuseix/Competitive_Programming_Buckup"')
     system('git add --all')
     system('git commit -m "first commit"')
     system('git push -u origin master')

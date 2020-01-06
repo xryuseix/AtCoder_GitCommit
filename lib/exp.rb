e8aa75e45902e78e39d2f20f66565f6d7a9f7f30
xryuseix <ryusei143.shootingstar@gmail.com>
2020-01-06 21:05:57 +0900

    test

--- lib/exp.rb
+++ lib/exp.rb
@@ -1,14 +1,14 @@
 require 'pty'
 require 'expect'
-cmd = 'git push origin master'
-  PTY.getpty(cmd) do | i,o |
-    o.sync = true
-    i.expect(/Username for 'https:\/\/github\.com':/, 10) { |line| #入力プロンプトくるまでreadline繰り返す
-      puts line
-      o.puts ENV['Git_ID']
-      o.flush
-    }
-    while( i.eof? == false )
-      puts i.gets
-    end
-  end
+# cmd = 'git push origin master'
+# PTY.getpty(cmd) do | i,o |
+#   o.sync = true
+#   i.expect(/Username for 'https:\/\/github\.com':/, 10) { |line| #入力プロンプトくるまでreadline繰り返す
+#     puts line
+#     o.puts ENV['Git_ID']
+#     o.flush
+#   }
+#   while( i.eof? == false )
+#     puts i.gets
+#   end
+# end

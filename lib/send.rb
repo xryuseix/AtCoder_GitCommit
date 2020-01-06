2d6c311574228ddbd5f0c86286dd4fe00ad7ce84
xryuseix <ryusei143.shootingstar@gmail.com>
2020-01-06 21:52:15 +0900

    test

--- lib/send.rb
+++ lib/send.rb
@@ -19,9 +19,9 @@ class Send
    
     system("cd storage;git add --all")
 
-    system("cd storage;git commit -m '#{lastday.year}/#{lastday.month}/#{lastday.day}\'s submit #{rand(100000)}'")
-    cmd = 'cd storage;git push origin master'
-    exec_as_root(cmd)
+    system("cd storage;git commit -m '#{lastday.year}/#{lastday.month}/#{lastday.day}\'s submit rand=#{rand(100000)}'")
+    # cmd = 'cd storage;git push origin master'
+    # exec_as_root(cmd)
 
     
     
